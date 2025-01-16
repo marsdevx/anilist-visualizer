bae024064aabfdf4d6b0a5b5515c8e61b1d39f85
GNUS <60074926+theMark001@users.noreply.github.com>
2025-01-16 13:57:46 +0100

    docs: add help

--- get_url.py
+++ get_url.py
@@ -15,8 +15,35 @@
 import sys
 import webbrowser
 
-if len(sys.argv) < 2:
-  print("Usage: python get_url.py <id>")
+def print_help():
+    print("""
+    AniList Visualizer - A tool for visualizing your AniList data.
+
+    Usage:
+    1. Create Your AniList Client:
+        - Go to https://anilist.co/settings/developer
+        - Click "Create New Client."
+        - Fill in the "name" field (use any name) and leave the "redirect URL" blank.
+        - Click "Save" and copy the Client ID (a series of five numbers under the "ID" field).
+
+    2. Generate the Authorization URL:
+        Run the following command, replacing <id> with the Client ID:
+        python3 get_url.py <id>
+        This will generate a URL for authorization.
+
+    3. Authorize the Application:
+        - Open the generated URL in your browser.
+        - Click "Authorize" on the AniList authorization page.
+        - Copy the URL from the address bar.
+
+    4. Run the Main Program:
+        Run the following command, replacing <url> with the copied URL:
+        python3 anilist_viz.py <url>
+        This will fetch your AniList data and display the visualization.
+    """)
+
+if len(sys.argv) != 2:
+  print_help()
   sys.exit(1)
 
 id = sys.argv[1]
